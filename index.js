2eb84f481fdc0ffd1bc7acb784063bf1dca1cc7f
rubis-design <rubis.smm@gmail.com>
2024-09-02 23:05:39 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+//1. Простое объявление переменной
+let gretting = 'Hello, World!';
+console.log(gretting);
+
+//2. Работа с числами
+let a = 5;
+let b = 10;
+let sum = a + b;
+console.log(sum);
+
+//3.Изменение значения переменной
+let name = 'Alice';
+name = 'Bob';
+console.log(name);
+
+//Типы данных
+//1. Задача: Определение типа данных
+let str = 'Hello';
+let num = 42;
+let boolean = true;
+
+console.log(typeof srt);
+console.log(typeof num);
+console.log(typeof boolean);
+
+//2. Задача: Преобразование данных в строку
+let age = 25;
+console.log(String(age));
+
+//3. Задача: Объединение строк
+let firstName = 'John';
+let lastName = 'Doe';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+//Преобразование типов
+//1.Задача: Преобразование строки в число
+let srtNumber = '100';
+console.log(Number(srtNumber));
+
+//2. Задача: Преобразование числа в строку
+let score = 95;
+console.log(String(score));
+
+//3. Задача: Преобразование булевого значения в строку
+let isLoggedIn = true;
+console.log(Boolean(isLoggedIn));
